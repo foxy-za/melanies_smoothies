e82a8395418372dc011a2d7c99d83694db0eb2bc
foxy-za <jraathza@gmail.com>
2025-10-30 16:06:23 +0200

    Display JSON response for watermelon API

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,4 @@ if ingredients_list:
 # section to display nutritional information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
